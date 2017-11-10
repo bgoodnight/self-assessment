1157ac0f153152ba9e5a572ef5a3aafe6c26dabe
darbosama <darbosama@gmail.com>
2017-11-10 15:06:06 -0500

    Created automatic report creation

---
+++ reports.R
@@ -0,0 +1,14 @@
+library(knitr)
+library(markdown)
+library(rmarkdown)
+
+survey = read.csv('data.csv', as.is = T)
+survey = survey[-1,]
+survey = survey[-1,]
+
+for (individual in unique(rownames(survey))){
+  rmarkdown::render('reports.Rmd',  # file 2
+                    output_file =  paste("report_", survey[individual,'Q2.2'], survey[individual,'Q2.1'], '_', Sys.Date(), ".pdf", sep=''), 
+                    output_dir = '~')
+}
+
