f376c62bd949dbe33ea677e39cb3cbb71e7711c8
Bradley Goodnight <darbosama@gmail.com>
2018-01-03 11:04:46 -0500

    Updates based on feedback

--- reports.R
+++ reports.R
@@ -7,7 +7,7 @@ survey = survey[-1,]
 survey = survey[-1,]
 
 for (individual in unique(rownames(survey))){
-  rmarkdown::render('reports.Rmd',  # file 2
+  rmarkdown::render('combined.Rmd',  # file 2
                     output_file =  paste("report_", survey[individual,'Q2.2'], survey[individual,'Q2.1'], '_', Sys.Date(), ".pdf", sep=''), 
                     output_dir = '~')
 }
