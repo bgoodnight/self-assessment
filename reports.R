6ac3ce7c8282da9a65389befba38d38a281278eb
Goodnight <knz1@cdc.gov>
2018-01-04 15:02:53 -0500

    Updated text based on feedback

--- reports.R
+++ reports.R
@@ -8,7 +8,6 @@ survey = survey[-1,]
 
 for (individual in unique(rownames(survey))){
   rmarkdown::render('combined.Rmd',  # file 2
-                    output_file =  paste("report_", survey[individual,'Q2.2'], survey[individual,'Q2.1'], '_', Sys.Date(), ".pdf", sep=''), 
-                    output_dir = '~\Reports')
+                    output_file =  paste("report_", survey[individual,'Q2.2'], survey[individual,'Q2.1'], '_', Sys.Date(), ".pdf", sep=''))
 }
 
